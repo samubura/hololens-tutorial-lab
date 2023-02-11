0182fb334705e7a60c5633d773bcbeb00deb8b11
samubura <samuele.bura98@gmail.com>
2023-02-12 00:19:25 +0100

    Spawning cubes

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefab;
+
+    public void NewCube()
+    {
+        GameObject.Instantiate(prefab, this.transform.position, Quaternion.identity);
+    }
+}
